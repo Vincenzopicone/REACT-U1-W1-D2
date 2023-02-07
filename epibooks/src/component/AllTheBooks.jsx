246a56c82144f3a7579c53968659c1fc1df4afe6
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-02-07 17:19:19 +0100

    fix

--- epibooks/src/component/AllTheBooks.jsx
+++ epibooks/src/component/AllTheBooks.jsx
@@ -7,7 +7,7 @@ function CardEpibook() {
         <h2 className='text-center'> FANTASY </h2>
         {Fantasy.map((book)=> {
             return (
-                <Card className='col-2 m-2 rounded' key={book.asin}>
+                <Card className='col-2 m-2 shadow p-3 mb-5 bg-body-tertiary rounded' key={book.asin}>
                 <Card.Img variant="fluid" src={book.img}/>
                 <Card.Body >
                   <Card.Title>{book.title}</Card.Title>
